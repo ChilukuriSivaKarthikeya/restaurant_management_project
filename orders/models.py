cb190e6d0f250525c824561b630af9390f3b1bfb
Chilukuri Siva Karthikeya <86362756+ChilukuriSivaKarthikeya@users.noreply.github.com>
2025-08-13 15:53:55 +0530

    17724

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Orders(models.Modal):
+    STATUS_CHOICES=[
+        ('PENDING','Pending'),
+        ('PROCESSING','Processing'),
+        ('COMPLETED','Completed'),
+        ('CANCELED','Canceled')
+    ]
+    customer=models.ForeignKey(User,on_delete=models.CASCADE)
+    order_items=models.ForeignKey(Menu)
+    total_amount=models.DecimalField(max_digits=10,decimal_places=2)
+    order_status=models.CharField(max_length=20,choices=STATUS_CHOICES,default='PENDING')
+
+    def __str__(self):
+        return self.customer.username
